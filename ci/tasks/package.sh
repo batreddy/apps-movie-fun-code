24955d00a6c83485fe062617de26e82f6c92d4b8
Manish Mishra <manish.mishra@globalfoundries.com>
2017-08-31 10:16:57 -0600

    modified files

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -2,19 +2,19 @@
 
 set -e +x
 
-pushd apps-movie-service-source
+pushd apps-movie-service-code
   echo "Packaging JAR"
   ./mvnw clean package -DskipTests
 popd
 
-jar_count=`find apps-movie-service-source/target -type f -name *.jar | wc -l`
+jar_count=`find apps-movie-fun-code/target -type f -name *.jar | wc -l`
 
 if [ $jar_count -gt 1 ]; then
   echo "More than one jar found, don't know which one to deploy. Exiting"
   exit 1
 fi
 
-find apps-movie-service-source/target -type f -name *.jar -exec cp "{}" package-output/apps-movie-service.jar \;
+find apps-movie-fun-code/target -type f -name *.jar -exec cp "{}" package-output/apps-movie-service.jar \;
 
 echo "Done packaging"
 exit 0

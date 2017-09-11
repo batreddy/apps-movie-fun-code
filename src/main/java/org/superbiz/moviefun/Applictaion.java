c8df15c6e3a68bd4853bf6baa002409a017fafc2
Manish Mishra <manish.mishra@globalfoundries.com>
2017-09-11 10:44:06 -0600

    modified files

---
+++ src/main/java/org/superbiz/moviefun/Applictaion.java
@@ -0,0 +1,18 @@
+package org.superbiz.moviefun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ctsuser1 on 9/11/17.
+ */
+
+@SpringBootApplication
+public class Applictaion {
+    public static void main(String[] args) {
+        SpringApplication.run(Applictaion.class, args);
+    }
+
+}
+
+

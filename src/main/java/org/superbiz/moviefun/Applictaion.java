58da478907d6c3ccccc813ef97765a026880b4e4
Manish Mishra <manish.mishra@globalfoundries.com>
2017-09-11 14:15:02 -0600

    updated files

--- src/main/java/org/superbiz/moviefun/Applictaion.java
+++ src/main/java/org/superbiz/moviefun/Applictaion.java
@@ -2,6 +2,8 @@ package org.superbiz.moviefun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by ctsuser1 on 9/11/17.
@@ -11,6 +13,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Applictaion {
     public static void main(String[] args) {
         SpringApplication.run(Applictaion.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(ActionServlet actionServlet){
+        return new ServletRegistrationBean(actionServlet, "/moviefun/*");
     }
 
 }
